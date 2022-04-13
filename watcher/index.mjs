fd3d91ed4195206d902f38ce4755d082530f57a2
Zach Sents <zachsents@gmail.com>
2022-04-12 23:39:44 -0600

    Used RxJS for concurrency pipelines

--- watcher/index.mjs
+++ watcher/index.mjs
@@ -1,87 +1,71 @@
 import fs from 'fs/promises'
 import path from 'path'
+import { from, fromEventPattern, mergeMap, filter } from 'rxjs'
+import { GridFSBucket } from 'mongodb'
+import mongoUUID from 'uuid-mongodb'
+import { pipeline } from 'stream/promises'
+
 
 import db from './mongo.mjs'
 import Status from './status.mjs'
+import { request } from './request.mjs'
 
-// Just for mock job
-import crypto from 'crypto'
 
 const jobsCollection = db.collection(process.env.JOB_COLLECTION)
 
-// Set up watch stream
-jobsCollection
-    // .watch({ fullDocument: 'updateLookup' })
-    .watch()
-    .on('change', async ({operationType}) => {
-    
-    // only interested in new and updated documents
-    if(operationType !== 'insert' && operationType !== 'update')
-        return
-
-    console.log('Detected document change.')
-
-    // check concurrency limit
-    let activeJobs = await jobsCollection.countDocuments({ status: Status.ACTIVE })
-    if(activeJobs < process.env.MAX_CONCURRENT_JOBS) {
-        
-        // grab top of FIFO stack
-        let nextJob = (await jobsCollection
-            .find({ status: Status.WAITING })
-            .sort({ _id: -1 })
-            .limit(1)
-            .toArray())[0]
-        
-        // check if anything was returned
-        if(nextJob) {
-
-            console.log('\tRunning job:', nextJob._id)
-
-            // update job with Active status and time of execution
-            jobsCollection.updateOne({
-                _id: nextJob._id
-            }, {
-                $set: { status: Status.ACTIVE },
-                $currentDate: { executionTime: true }
-            })
-
-            // read in associated file
-            let filePath = path.join(process.cwd(), process.env.JOB_STORAGE_FOLDER, nextJob.bucketFileName)
-            let fileContent = (await fs.readFile(filePath)).toString()
-            console.log('\tGrabbed file from', filePath)
-
-            // run job
-            // TO DO: make iBioSim API request here
-            const results = await runMockJob(fileContent)
-
-            console.log('\tFinished job:', nextJob._id)
-            console.log('\tResult:', results)
-
-            // update job with results and Complete status
-            jobsCollection.updateOne({
-                _id: nextJob._id
-            }, {
-                $set: { 
-                    status: Status.COMPLETE,
-                    results
-                },
-                $currentDate: { completionTime: true }
-            })
-        }
-        else
-            console.log('Queue empty!')
-        
-    }
-    else
-        console.log('At concurrency limit. Ignoring event.')
+// Create observable
+const documents = fromEventPattern(handler => {
+    jobsCollection.watch().on('change', handler)
 })
 
+// Set up pipeline
+documents
+    .pipe(
+        filter(event => event.operationType == 'insert'),
+        mergeMap(event => from(runJob(event)), process.env.MAX_CONCURRENT_JOBS)
+    )
+    .subscribe(
+        results => console.log('Done. Stored results in:\n', results)
+    )
+
+
 /*
-*   Dummy function that mocks a simulation
-*   with a timeout
+*   Run the job through iBioSim API
 */
-function runMockJob(text = 'hello', time = 5000) {
-    return new Promise(resolve => setTimeout(
-        () => resolve(crypto.createHash('md5').update(text).digest('hex')),
-    time))
+async function runJob({ fullDocument }) {
+
+    // update status of document
+    jobsCollection.updateOne({
+        _id: fullDocument._id
+    }, {
+        $set: { status: Status.ACTIVE },
+        $currentDate: { executionTime: true }
+    })
+
+    // read in associated file
+    let filePath = path.join(process.cwd(), process.env.JOB_STORAGE_FOLDER, fullDocument.bucketFileName)
+    let fileBuffer = await fs.readFile(filePath)
+
+    // run job
+    const response = await request(fullDocument.type, fullDocument.options, fullDocument.originalFileName, fileBuffer)
+
+    // store result in GridFS
+    const bucket = new GridFSBucket(db)
+    const uploadStream = bucket.openUploadStream(`${mongoUUID.v4().toString()}.zip`, {
+        metadata: { field: 'job', value: fullDocument._id }
+    })
+    await pipeline(response.body, uploadStream)
+
+    // update document with status and results
+    jobsCollection.updateOne({
+        _id: fullDocument._id
+    }, {
+        $set: {
+            status: Status.COMPLETE,
+            results: uploadStream.id
+        },
+        $currentDate: { completionTime: true }
+    })
+
+    return uploadStream.id
 }
---
+++ watcher/request.mjs
@@ -0,0 +1,48 @@
+import path from 'path'
+import FormData from 'form-data'
+import fetch from 'node-fetch'
+import { pipeline } from 'stream/promises'
+import fs from 'fs/promises'
+
+
+export async function convert(options, filename, fileBuffer) {
+    request('convert', options, filename, fileBuffer)
+}
+
+export async function analyze(options, filename, fileBuffer) {
+    request('analyze', options, filename, fileBuffer)
+}
+
+export async function convertAndAnalyze(options, filename, fileBuffer) {
+    request('convert_and_simulate', options, filename, fileBuffer)
+}
+
+export async function request(endpoint, options, filename, fileBuffer) {
+
+    // format options & create request URL
+    const params = Object.entries(options).map(e => e.join('=')).join('&')
+    const url = path.join(process.env.IBIOSIM_URL, `${endpoint}?${params}`)
+
+    // prepare form body
+    let formData = new FormData()
+    formData.append('file', fileBuffer, { filename })
+
+    // make the request
+    let response
+    try {
+        response = await fetch(url, {
+            header: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: formData,
+            method: 'post'
+        })
+    }
+    catch (e) {
+        console.error(e.toString())
+        response = { body: e.toString() }
+    }
+
+    return response
+}
+
